f4be6cedeef66a319709dbd4315b4d9b7ee2ccca
Łukasz Rydziński <rlukasz1996@gmail.com>
2020-06-17 04:16:26 +0200

    || Root scene update

---
+++ ArkanoidRemake/Assets/Scripts/_Root/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameController Instance => this;
+
+    public GameData Data { get; private set; }
+    public SaveController Save { get; private set; }
+    public AudioController Audio { get; private set; }
+
+    private void Start()
+    {
+        Data = GetComponentInChildren<GameData>();
+        Save = GetComponentInChildren<SaveController>();
+        Audio = GetComponentInChildren<AudioController>();
+    }
+
+
+}
