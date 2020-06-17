857dc1450ff5226ecf5afeef0faee142ab83d950
Łukasz Rydziński <rlukasz1996@gmail.com>
2020-06-17 03:12:01 +0200

    || Created UI Menu

---
+++ ArkanoidRemake/Assets/Scripts/BGMove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGMove : MonoBehaviour
+{
+    private RawImage rawImage => GetComponent<RawImage>();
+    private float valueX, valueY;
+    private float time = 0.01f;
+
+    private void Start() => StartCoroutine(MoveBG(time));
+
+    IEnumerator MoveBG(float time)
+    {
+        yield return new WaitForSeconds(time);
+        valueX = rawImage.uvRect.x;// - 0.001f;
+        valueY = rawImage.uvRect.y + 0.0002f;
+
+        rawImage.uvRect = new Rect(valueX, valueY, 1, 1);
+        StartCoroutine(MoveBG(time));
+    }
+}
